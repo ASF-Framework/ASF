fb249cacacd3f86a36c5dd26ef95b4b95997e1c1
aqa510415008 <510415008@qq.com>
2019-02-13 15:29:13 +0800

    添加JwtBearer权限验证和JwtToken生成

---
+++ src/ASF.Infrastructure/Anticorrsives/AccessTokenGenerate.cs
@@ -0,0 +1,60 @@
+﻿using ASF.Domain.Values;
+using ASF.Infrastructure.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ASF.Infrastructure.Anticorrsives
+{
+    public class AccessTokenGenerate : IAccessTokenGenerate
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ASFOptions _options;
+
+        public AccessTokenGenerate(IHttpContextAccessor httpContextAccessor, IOptions<ASFOptions> options)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _options = options.Value;
+        }
+
+        /// <summary>
+        /// 生成Token
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <returns></returns>
+        public AccessToken Generate(Dictionary<string, string> claims)
+        {
+            TokenOptions tokenOpt = _options.JwtToken;
+            var now = DateTime.UtcNow;
+            var expires = DateTime.UtcNow.AddMinutes(tokenOpt.Expires);
+            var claimList = new List<Claim>();
+            var issuer = tokenOpt.Issuer;
+            var audience = tokenOpt.Audience;
+            var signingCredentials = tokenOpt.SigningCredentials;
+            if (string.IsNullOrEmpty(issuer))
+            {
+                issuer = _httpContextAccessor.HttpContext.Request.Host.ToString();
+            }
+            foreach (var claim in claims)
+            {
+                claimList.Add(new Claim(claim.Key, claim.Value));
+            }
+
+            var jwt = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claimList.ToArray(),
+                notBefore: now,
+                expires: expires,
+                signingCredentials: signingCredentials
+            );
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            return new AccessToken(encodedJwt, "Bearer", expires);
+        }
+
+      
+    }
+}
---
+++ src/ASF.Infrastructure/DependencyInjection/ASFOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace ASF.Infrastructure.DependencyInjection
+{
+    public class ASFOptions
+    {
+        public ASFOptions()
+        {
+            this.JwtToken = new TokenOptions();
+        }
+        public TokenOptions JwtToken { get; set; }
+    }
+}

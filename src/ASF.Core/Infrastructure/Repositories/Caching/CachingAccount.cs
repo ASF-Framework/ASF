98370c97bce211b34e10bf51721440832c78a343
liuchao <a641545621@163.com>
2019-02-19 17:06:42 +0800

    缓存权限角色

--- src/ASF.Core/Infrastructure/Repositories/Caching/CachingAccount.cs
+++ src/ASF.Core/Infrastructure/Repositories/Caching/CachingAccount.cs
@@ -28,7 +28,12 @@ namespace ASF.Infrastructure.Repositories
 
         public async Task<Account> AddAsync(Account entity)
         {
-            return await _repository.AddAsync(entity);
+            var account = await _repository.AddAsync(entity);
+            //更新缓存
+            var list = await _accountCache.GetAsync(_cacheKey, _duration, async () => await _repository.GetList(), _logger);
+            list.Add(account);
+            await _accountCache.SetAsync(_cacheKey, list, _duration);
+            return account;
         }
 
         public async Task<Account> GetAsync(PhoneNumber telephone)
@@ -58,14 +63,14 @@ namespace ASF.Infrastructure.Repositories
         public async Task<(IList<Account> Accounts, int TotalCount)> GetList(AccountInfoListPagedRequestDto requestDto)
         {
             var list = await _accountCache.GetAsync(_cacheKey, _duration, async () => await _repository.GetList(), _logger);
-            var queryable=list.Where(w => w.IsDeleted == requestDto.IsDeleted);
+            var queryable = list.Where(w => w.IsDeleted == requestDto.IsDeleted);
 
             if (!string.IsNullOrEmpty(requestDto.Vague))
             {
                 queryable = queryable
                     .Where(w => w.Id.ToString().Contains(requestDto.Vague)
                     || w.Name.Contains(requestDto.Vague)
-                    ||w.Username.Contains(requestDto.Vague)
+                    || w.Username.Contains(requestDto.Vague)
                     );
             }
             if (requestDto.Status == 1)
@@ -74,7 +79,7 @@ namespace ASF.Infrastructure.Repositories
                 queryable = queryable.Where(w => w.Status == AccountStatus.NotAllowedLogin);
 
             var result = queryable.OrderByDescending(p => p.CreateInfo.CreateTime);
-            return (result.Skip((requestDto.SkipPage - 1) * requestDto.PagedCount).Take(requestDto.PagedCount).ToList(),result.Count());
+            return (result.Skip((requestDto.SkipPage - 1) * requestDto.PagedCount).Take(requestDto.PagedCount).ToList(), result.Count());
         }
 
         public async Task<IList<Account>> GetList()
@@ -107,11 +112,33 @@ namespace ASF.Infrastructure.Repositories
         public async Task ModifyAsync(Account account)
         {
             await _repository.ModifyAsync(account);
+
+            //更新缓存
+            var list = await _accountCache.GetAsync(_cacheKey, _duration, async () => await _repository.GetList(), _logger);
+            var entity = list.FirstOrDefault(f => f.Id == account.Id);
+            if (entity != null)
+            {
+                list.Remove(entity);
+                var model = await _repository.GetAsync(account.Id);
+                list.Add(model);
+                await _accountCache.SetAsync(_cacheKey, list, _duration);
+            }
         }
 
         public async Task RemoveAsync(int primaryKey)
         {
             await _repository.RemoveAsync(primaryKey);
+
+            //更新缓存
+            var list = await _accountCache.GetAsync(_cacheKey, _duration, async () => await _repository.GetList(), _logger);
+            var entity = list.FirstOrDefault(f => f.Id == primaryKey);
+            if (entity != null)
+            {
+                list.Remove(entity);
+                var model = await _repository.GetAsync(primaryKey);
+                list.Add(model);
+                await _accountCache.SetAsync(_cacheKey, list, _duration);
+            }
         }
     }
 }
--- src/ASF.Infrastructure/DependencyInjection/ASFBuilderExtensions.cs
+++ src/ASF.Infrastructure/DependencyInjection/ASFBuilderExtensions.cs
@@ -63,8 +63,14 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<IAccountRepository, CachingAccount<AccountRepository>>();
 
             services.AddScoped<ILoggingRepository, LogInfoRepository>();
-            services.AddScoped<IPermissionRepository, PermissionRepository>();
-            services.AddScoped<IRoleRepository, RoleRepository>();
+
+            //services.AddScoped<IPermissionRepository, PermissionRepository>();
+            services.TryAddScoped(typeof(PermissionRepository));
+            services.AddScoped<IPermissionRepository, CachingPermission<PermissionRepository>>();
+
+            //services.AddScoped<IRoleRepository, RoleRepository>();
+            services.TryAddScoped(typeof(RoleRepository));
+            services.AddScoped<IRoleRepository, CachingRole<RoleRepository>>();
         }
         /// <summary>
         /// 注入防腐层

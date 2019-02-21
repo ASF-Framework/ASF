e0da44da6be490706602d6dae25eea51c7934e3a
aqa510415008 <510415008@qq.com>
2019-02-21 14:14:49 +0800

    添加个人修改昵称和头像Api

---
+++ src/ASF.Core/Application/DTO/Account/AccountModifyNameOrAvatarRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASF.Application.DTO
+{
+    public class AccountModifyNameOrAvatarRequestDto : IDto
+    {
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        [ MaxLength(20)]
+        public string Name { get; set; }
+        /// <summary>
+        /// 头像地址
+        /// </summary>
+        [MaxLength(225)]
+        public string Avatar { get; set; }
+        /// <summary>
+        /// 转换Json字符串
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}

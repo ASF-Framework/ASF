aaf27cbf441b2f7f773e12077e3b0c48690d6551
Soledad <adobe8@163.com>
2019-02-24 14:33:12 +0800

    新增详情页面

--- app/src/config/router.config.js
+++ app/src/config/router.config.js
@@ -95,7 +95,28 @@ export const asyncRouterMap = [
           }
         ]
       },
-
+      // profile
+      {
+        path: '/profile',
+        name: 'profile',
+        component: RouteView,
+        redirect: '/profile/basic',
+        meta: { title: '详情页', icon: 'profile', permission: [ 'profile' ] },
+        children: [
+          {
+            path: '/profile/basic',
+            name: 'ProfileBasic',
+            component: () => import('@/views/profile/basic/Index'),
+            meta: { title: '基础详情页', permission: [ 'profile' ] }
+          },
+          {
+            path: '/profile/advanced',
+            name: 'ProfileAdvanced',
+            component: () => import('@/views/profile/advanced/Advanced'),
+            meta: { title: '高级详情页', permission: [ 'profile' ] }
+          }
+        ]
+      },
       // account
       {
         path: '/account',

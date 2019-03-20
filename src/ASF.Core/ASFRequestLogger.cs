05e908c6b929a0dd651e84ec0c067dcb5a815820
aqa510415008 <510415008@qq.com>
2019-03-20 15:42:07 +0800

    修复日志记录Bug

--- src/ASF.Core/ASFRequestLogger.cs
+++ src/ASF.Core/ASFRequestLogger.cs
@@ -5,7 +5,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Middleware;
+using System;
 using System.IO;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,14 +20,18 @@ namespace ASF
     /// </summary>
     public class ASFRequestLogger
     {
-        private readonly HttpContext context;
+        private readonly DownstreamContext context;
         private readonly Permission permission;
-        public ASFRequestLogger(HttpContext context, Permission permission)
+        private readonly HttpContext httpContext;
+        private readonly IServiceProvider serviceProvider;
+        public ASFRequestLogger(DownstreamContext context, Permission permission)
         {
             this.context = context;
             this.permission = permission;
+            this.httpContext = context.HttpContext;
+            this.serviceProvider = httpContext.RequestServices;
         }
-        public static Task Record(HttpContext context, Permission permission)
+        public static Task Record(DownstreamContext context, Permission permission)
         {
             return new ASFRequestLogger(context, permission).Record();
         }
@@ -32,86 +39,14 @@ namespace ASF
         /// 记录日志
         /// </summary>
         /// <param name="httpContext"></param>
-        public Task Record()
+        public async Task Record()
         {
-            var serviceProvider = context.RequestServices;
-            var response = context.Response;
-             
-            if (!response.Body.CanRead || !response.Body.CanSeek)
-            {
-                response.Body = new MemoryWrappedHttpResponseStream(response.Body);
-            }
-            context.Response.OnCompleted(async o =>
-            {
-                var c = o as HttpContext;
-                if (c != null)
-                {
-                    //获取所有请求参数和响应数据
-                    var requestData = await this.GetRequestData(context.Request);
-                    var responseData = await this.GetResponseData(response);
-                    serviceProvider.GetRequiredService<LogOperateRecordService>().Record(permission, requestData, responseData);
-                }
-            }, context);
-            return Task.CompletedTask;
-        }
-        /// <summary>
-        /// 获取请求参数
-        /// </summary>
-        /// <param name="httpContext"></param>
-        /// <returns></returns>
-        private async Task<string> GetRequestData(HttpRequest request)
-        {
-            if (request.ContentLength > 0)
-            {
-                await this.EnableRewindAsync(request).ConfigureAwait(false);
-                var encoding = this.GetEncoding(request.ContentType);
-                return await this.ReadStreamAsync(request.Body, encoding).ConfigureAwait(false);
-            }
-            return "";
-        }
-        /// <summary>
-        /// 获取响应数据
-        /// </summary>
-        /// <param name="response"></param>
-        /// <returns></returns>
-        private async Task<string> GetResponseData(HttpResponse response)
-        {
-            if (response.Body.Length > 0)
-            {
-                var encoding = this.GetEncoding(response.ContentType);
-                response.Body.Seek(0, SeekOrigin.Begin);
-                return await this.ReadStreamAsync(response.Body, encoding).ConfigureAwait(false);
-            }
-            return "";
-        }
-        private Encoding GetEncoding(string contentType)
-        {
-            var requestMediaType = contentType == null ? default(MediaType) : new MediaType(contentType);
-            var requestEncoding = requestMediaType.Encoding;
-            if (requestEncoding == null)
-            {
-                requestEncoding = Encoding.UTF8;
-            }
-            return requestEncoding;
-        }
-        private async Task EnableRewindAsync(HttpRequest request)
-        {
-            if (!request.Body.CanSeek)
-            {
-                request.EnableBuffering();
+            var responseData = await context.DownstreamResponse.Content.ReadAsStringAsync();
+            var requestData = await context.DownstreamRequest.ToHttpRequestMessage().Content.ReadAsStringAsync();
 
-                await request.Body.DrainAsync(CancellationToken.None);
-                request.Body.Seek(0L, SeekOrigin.Begin);
-            }
+            serviceProvider.GetRequiredService<LogOperateRecordService>().Record(permission, requestData, responseData);
+            return ;
         }
-        private async Task<string> ReadStreamAsync(Stream stream, Encoding encoding)
-        {
-            using (StreamReader sr = new StreamReader(stream, encoding, true, 1024, true))//这里注意Body部分不能随StreamReader一起释放
-            {
-                var str = await sr.ReadToEndAsync();
-                stream.Seek(0, SeekOrigin.Begin);//内容读取完成后需要将当前位置初始化，否则后面的InputFormatter会无法读取
-                return str;
-            }
-        }
+       
     }
 }
--- src/ASF.Core/ASFRequestLoggerMiddleware.cs
+++ src/ASF.Core/ASFRequestLoggerMiddleware.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Ocelot.Middleware;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ASF
@@ -16,6 +17,13 @@ namespace ASF
         {
             await next.Invoke();
 
+            //判断是否响应成功
+            if (context.DownstreamResponse == null || 
+                context.DownstreamResponse.StatusCode != HttpStatusCode.OK)
+            {
+                return;
+            }
+
             //开始记录日志
             var serviceProvider = context.HttpContext.RequestServices;
             var logger = serviceProvider.GetRequiredService<ILogger<ASFRequestLoggerMiddleware>>();
@@ -28,7 +36,7 @@ namespace ASF
                         //判断是否需要记录日志
                         if (per.IsLogger)
                         {
-                            await ASFRequestLogger.Record(context.HttpContext, per);
+                            await ASFRequestLogger.Record(context, per);
                         }
                     }
                     else
@@ -47,7 +55,7 @@ namespace ASF
                 return;
             }
         }
-      
+
 
     }
 }

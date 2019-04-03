e8ee70b7a1d5077d744a97e7de85a66ce75fa055
lfzm <510415008@qq.com>
2019-04-03 14:37:45 +0800

    add exception view

---
+++ app/src/views/exception/500.vue
@@ -0,0 +1,15 @@
+<template>
+  <exception-page type="500" />
+</template>
+
+<script>
+import { ExceptionPage } from '@/components'
+export default {
+  components: {
+    ExceptionPage
+  }
+}
+</script>
+
+<style scoped>
+</style>

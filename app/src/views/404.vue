9f2f6dcdbdd1b2b5db04c8668421f6016d776938
lfzm <510415008@qq.com>
2019-04-03 11:53:30 +0800

    add exception view

--- app/src/views/404.vue
+++ app/src/views/404.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    404 page
-  </div>
+  <exception-page type="404" />
 </template>
 
 <script>
+import { ExceptionPage } from '@/components'
 export default {
-  name: '404'
+  components: {
+    ExceptionPage
+  }
 }
 </script>
 
 <style scoped>
-
 </style>

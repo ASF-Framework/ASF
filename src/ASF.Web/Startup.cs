71af2fb32cc3c5be05e03d8268e8038e99aad590
阿凌 <510415008@qq.com>
2019-08-12 16:19:16 +0800

    生成Swagger 文档

--- src/ASF.Web/Startup.cs
+++ src/ASF.Web/Startup.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 
 namespace ASF.Web
 {
@@ -30,6 +32,17 @@ namespace ASF.Web
                 build.AddDbContext(b => b.UseSqlite(dbConnectionString), _env);
                 build.AddAuthenticationJwtBearer();
             });
+
+
+            //注册Swagger生成器，定义一个和多个Swagger 文档
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ASF", Version = "v1" });
+                // 为 Swagger JSON and UI设置xml文档注释路径
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//获取应用程序所在目录（绝对，不受工作目录影响，建议采用此方法获取路径）
+                var xmlPath = Path.Combine(basePath, "ASF.Core.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +54,14 @@ namespace ASF.Web
             }
             app.UseFileServer();
 
-            app.ASFInitDatabase();
+            //启用中间件服务生成Swagger作为JSON终结点
+            app.UseSwagger();
+            //启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ASF");
+            });
+            //app.ASFInitDatabase();
             app.UseASF().Wait();
         }
     }

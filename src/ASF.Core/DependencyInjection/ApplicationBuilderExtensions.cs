9bdb81da85a51de672b338d0b264a45b1fc02fea
AClumsy <510415008@qq.com>
2019-09-16 22:51:07 +0800

    fix：nginx代理时无法获取真实IP

--- src/ASF.Core/DependencyInjection/ApplicationBuilderExtensions.cs
+++ src/ASF.Core/DependencyInjection/ApplicationBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Ocelot.Middleware;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Builder
@@ -9,6 +10,24 @@ namespace Microsoft.AspNetCore.Builder
     {
         public static Task<IApplicationBuilder> UseASF(this IApplicationBuilder app)
         {
+            // Nginx 代理时获取真实 IP
+            app.Use((context, next) =>
+            {
+                var headers = context.Request.Headers;
+                try
+                {
+                    if (headers.ContainsKey("X-Forwarded-For"))
+                    {
+                        context.Connection.RemoteIpAddress = IPAddress.Parse(headers["X-Forwarded-For"].ToString().Split(',')[0]);
+                    }
+                }
+                finally
+                {
+                    next();
+                }
+                return Task.CompletedTask;
+            });
+
             return app.UseOcelot(opt =>
             {
                 opt.UseASF();
